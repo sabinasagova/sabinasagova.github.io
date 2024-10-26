59bae55a4c7cd70831bfdb49230bd81bbfefbea6
Sabína <sagovasabina@gmail.com>
2024-10-26 23:32:19 +0200

    iny gif

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Super, teším sa!";
   gif.src =
-    "https://media1.tenor.com/m/uI87R_DI-lQAAAAC/peach-goma-peach-and-goma.gif";
+    "https://media1.tenor.com/m/6rf6vbTHL2IAAAAC/peach-and-goma-urka.gif";
 });
-
-noBtn.addEventListener("mouseover", () => {
+  noBtn.addEventListener("mouseover", () => {
   const noBtnRect = noBtn.getBoundingClientRect();
   const maxX = window.innerWidth - noBtnRect.width;
   const maxY = window.innerHeight - noBtnRect.height;
